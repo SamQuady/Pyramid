fd3b9456de4df47e709f27d922d39c77ffd0f33c
Sam Quady <samquady1@gmail.com>
2021-03-17 11:51:35 -0700

    Added nodeOganizer Helper Function

--- tree-creation.js
+++ tree-creation.js
@@ -23,3 +23,28 @@ let pyramidValidator = (nodes) => {
     return result;
   }
 }
+
+let nodeOrganizer = (nodeArray) => {
+
+  let levels = [];
+  let level = 1;
+  let levelVals = 0;
+
+  for (let index = 0; index < nodeArray.length; index ++) {
+    let node = {};
+    node.val = nodeArray[index];
+    node.left = null;
+    node.right = null;
+    if (levelVals === level) {
+      level ++;
+      levelVals = 0;
+    }
+    if (levelVals === 0) {
+      levels.push([]);
+    }
+    levels[level - 1].push(node);
+    levelVals ++;
+  }
+
+  return levels;
+}
