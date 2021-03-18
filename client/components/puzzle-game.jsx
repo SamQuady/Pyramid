23a8d218f63db4541fe83fb105daf16f6426eeb7
Sam Quady <samquady1@gmail.com>
2021-03-18 15:34:21 -0700

    Basic Logic for Click Handler

--- client/components/puzzle-game.jsx
+++ client/components/puzzle-game.jsx
@@ -8,19 +8,47 @@ class PuzzleGame extends React.Component {
     this.state = {
       pyramid: this.props.pyramid,
       runningTotal: this.props.pyramid[0][0],
-      moves: ''
+      moves: '',
+      tree: Helpers.treeBuilder(this.props.pyramid.flat()),
+      clickPyramid: null
     };
-
     this.showAnswer = this.showAnswer.bind(this);
+    this.nodeClick = this.nodeClick.bind(this);
+    this.clickPyramidBuilder = this.clickPyramidBuilder.bind(this);
   }
 
   showAnswer() {
-    let nodeArray = this.state.pyramid.flat();
-    let tree = Helpers.treeBuilder(nodeArray);
-    let result = Helpers.pathFinder(tree, this.props.goal);
+    let result = Helpers.pathFinder(this.state.tree, this.props.goal);
     this.setState({moves: result, runningTotal: this.props.goal});
   }
 
+  nodeClick(row, position) {
+    if (this.state.clickPyramid === null) {
+      this.clickPyramidBuilder()
+    }
+    //still need to add some logic checking if the click is valid, ie down a row and either left or right
+    let clickPyramid = this.state.clickPyramid;
+    let runningTotal = this.state.runningTotal;
+    if (!clickPyramid[row][position]) {
+      clickPyramid[row][position] = 1;
+      runningTotal *= this.state.pyramid[row][position];
+    } else {
+      clickPyramid[row][position] = 0;
+      runningTotal /= this.state.pyramid[row][position];
+    }
+    this.setState({clickPyramid: clickPyramid, runningTotal: runningTotal});
+  }
+
+  clickPyramidBuilder() {
+    let clickPyramid = [];
+    for (let index = 0; index < this.props.pyramid.length; index ++) {
+      let clickRow = new Array(this.props.pyramid[index].length).fill(0);
+      clickPyramid.push(clickRow);
+    }
+    this.setState({clickPyramid: clickPyramid})
+    console.log(this.state.clickPyramid.length)
+  }
+  //need to feed column numer and index number to click handler
   render() {
     return (
       <div>
