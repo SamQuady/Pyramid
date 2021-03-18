2e055f602cd97bb8055c8b186d42457c3688df68
Sam Quady <samquady1@gmail.com>
2021-03-18 15:01:50 -0700

    Installed Styled Components, Printing Moves and Running Total on Show me Answer Click

--- client/components/puzzle-game.jsx
+++ client/components/puzzle-game.jsx
@@ -6,14 +6,19 @@ class PuzzleGame extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pyramid: this.props.pyramid
+      pyramid: this.props.pyramid,
+      runningTotal: this.props.pyramid[0][0],
+      moves: ''
     };
 
     this.showAnswer = this.showAnswer.bind(this);
   }
 
   showAnswer() {
-    console.log('going to use helpers here');
+    let nodeArray = this.state.pyramid.flat();
+    let tree = Helpers.treeBuilder(nodeArray);
+    let result = Helpers.pathFinder(tree, this.props.goal);
+    this.setState({moves: result, runningTotal: this.props.goal});
   }
 
   render() {
@@ -21,7 +26,15 @@ class PuzzleGame extends React.Component {
       <div>
         <div>{this.state.pyramid.map((row, index) => <PuzzleRow index={index} key={index} build={false} spaces={row} />)}</div>
         <div>
-          Goal: {this.props.goal}
+          <div>
+            Moves: {this.state.moves}
+          </div>
+          <div>
+            Running Total: {this.state.runningTotal}
+          </div>
+          <div>
+            Goal: {this.props.goal}
+          </div>
         </div>
         <div>
           <button onClick={this.showAnswer}>Show me the Answer!</button>
