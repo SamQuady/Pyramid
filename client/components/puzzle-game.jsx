84180eb4a405a8b8e90a4cc483f722648c815df5
Sam Quady <samquady1@gmail.com>
2021-03-18 23:49:39 -0700

    Styled Puzzle Page

--- client/components/puzzle-game.jsx
+++ client/components/puzzle-game.jsx
@@ -1,6 +1,44 @@
 import React from 'react';
 import PuzzleRow from './puzzle-row.jsx';
 import Helpers from './helpers/tree-helpers.js';
+import styled from 'styled-components';
+
+const GameTextFields = styled.span`
+font-family: Arial, Helvetica, sans-serif;
+font-size: 20px;
+text-align: center;
+padding-left: 60px;
+padding-right: 60px;
+`;
+
+const GameTextFieldHolder = styled.div`
+padding-top: 40px;
+text-align: center;
+`;
+
+const ShowAnswerButton = styled.button`
+line-height: 30px;
+margin-left: 20px;
+margin-right: 20px;
+background-color: white;
+border-radius: 2px;
+border-style: solid;
+border-color: rgb(184, 184, 184);
+border-width: 1px;
+color: rgb(41, 41, 41);
+cursor: pointer;
+font-family: "Roboto","Helvetica Neue","Helvetica","Arial",sans-serif;
+font-size: 14px;
+font-weight: 400;
+height: 34px;
+width: auto;
+&:hover {box-shadow: inset 0 0 3px #000000;}
+`;
+
+const ShowAnswerButtonHolder = styled.div`
+padding-top: 60px;
+text-align: center;
+`;
 
 class PuzzleGame extends React.Component {
   constructor(props) {
@@ -71,20 +109,20 @@ class PuzzleGame extends React.Component {
     return (
       <div>
         <div>{this.state.pyramid.map((row, index) => <PuzzleRow onClick={this.nodeClick} index={index} key={index} build={false} spaces={row} />)}</div>
-        <div>
-          <div>
+        <GameTextFieldHolder>
+          <GameTextFields>
             Moves: {this.state.moves}
-          </div>
-          <div>
+          </GameTextFields>
+          <GameTextFields>
             Running Total: {this.state.runningTotal}
-          </div>
-          <div>
+          </GameTextFields>
+          <GameTextFields>
             Goal: {this.props.goal}
-          </div>
-        </div>
-        <div>
-          <button onClick={this.showAnswer}>Show me the Answer!</button>
-        </div>
+          </GameTextFields>
+        </GameTextFieldHolder>
+        <ShowAnswerButtonHolder>
+          <ShowAnswerButton onClick={this.showAnswer}>Show me the Answer!</ShowAnswerButton>
+        </ShowAnswerButtonHolder>
       </div>
       )
   }
